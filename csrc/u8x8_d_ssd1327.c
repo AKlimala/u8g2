4a955901d48e63fa18f84ffe60c6eaf57a65022b
Alan Klimala <a.klimala@mbdsoft.pl>
2024-03-06 20:05:09 +0000

    Add SSD1327 with Windstar 128x64 display support.

--- csrc/u8x8_d_ssd1327.c
+++ csrc/u8x8_d_ssd1327.c
@@ -334,6 +334,102 @@ uint8_t u8x8_d_ssd1327_ws_96x64(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void
   return 0;
 }
 
+/*=============================================*/
+/*
+  Winstar WEO012864AA 128x64 OLED Display
+  https://www.winstar.com.tw/products/oled-module/graphic-oled-display/128x64-oled.html
+*/
+
+static const u8x8_display_info_t u8x8_ssd1327_winstar_128x64_display_info =
+{
+  /* chip_enable_level = */ 0,
+  /* chip_disable_level = */ 1,
+  
+  /* post_chip_enable_wait_ns = */ 20,
+  /* pre_chip_disable_wait_ns = */ 10,
+  /* reset_pulse_width_ms = */ 100, 	
+  /* post_reset_wait_ms = */ 100, 		/**/
+  /* sda_setup_time_ns = */ 100,		/* */
+  /* sck_pulse_width_ns = */ 100,	/*  */
+  /* sck_clock_hz = */ 4000000UL,	/* since Arduino 1.6.0, the SPI bus speed in Hz. Should be  1000000000/sck_pulse_width_ns */
+  /* spi_mode = */ 0,		/* active high, rising edge */
+  /* i2c_bus_clock_100kHz = */ 1,	/* use 1 instead of 4, because the SSD1327 seems to be very slow */
+  /* data_setup_time_ns = */ 40,
+  /* write_pulse_width_ns = */ 60,	
+  /* tile_width = */ 16,
+  /* tile_hight = */ 8,
+  /* default_x_offset = */ 0,		/* changed to 16, issue 1050 */
+  /* flipmode_x_offset = */ 16,		/* changed to 16, issue 1050 */
+  /* pixel_width = */ 128,
+  /* pixel_height = */ 64
+};
+
+static const uint8_t u8x8_d_ssd1327_winstar_128x64_init_seq[] = {
+    
+  U8X8_START_TRANSFER(),             	/* enable chip, delay is part of the transfer start */
+  
+  U8X8_CA(0x0fd, 0x012),		/* unlock display, usually not required because the display is unlocked after reset */
+  U8X8_C(0x0ae),		                /* display off */
+  
+  U8X8_CA(0x0d9, 0x000),		/* Pre-charge Period ??? */
+  
+  U8X8_CA(0x0a0, 0x042),		/* remap configuration */
+  U8X8_CA(0x0a1, 0x000),		/* display start line */  
+  U8X8_CA(0x0a2, 0x000),		/* display offset, shift mapping ram counter */
+  
+  U8X8_CA(0x0a8, 0x063),		/* multiplex ratio: 63* 1/64 duty */ /* changed to hex, issue 1050 */
+  
+  U8X8_CA(0x0ab, 0x001),		/* Enable internal VDD regulator (RESET) */
+  U8X8_CA(0x081, 0x053),		/* contrast, brightness, 0..128 */
+  
+  U8X8_CA(0x0b1, 0x047),                    /* phase length */  
+  //U8X8_CA(0x0b3, 0x001),		/* set display clock divide ratio/oscillator frequency  */			
+  U8X8_CA(0x0b3, 0x000),		/* set display clock divide ratio/oscillator frequency  */			
+  
+  U8X8_C(0x0b9),				/* use linear lookup table */
+
+  U8X8_CA(0x0bc, 0x007),                    /* pre-charge voltage level */
+  U8X8_CA(0x0be, 0x007),                     /* VCOMH voltage */
+  U8X8_CA(0x0b6, 0x004),		/* second precharge */
+  U8X8_CA(0x0d5, 0x062),		/* enable second precharge, internal vsl (bit0 = 0) */
+  
+  U8X8_C(0x0a4),				/* normal display mode */
+    
+  U8X8_END_TRANSFER(),             	/* disable chip */
+  U8X8_END()             			/* end of sequence */
+};
+
+uint8_t u8x8_d_ssd1327_ws_128x64(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+  if ( u8x8_d_ssd1327_96x96_generic(u8x8, msg, arg_int, arg_ptr) != 0 )
+    return 1;
+  if ( msg == U8X8_MSG_DISPLAY_SETUP_MEMORY )
+  {
+    u8x8_d_helper_display_setup_memory(u8x8, &u8x8_ssd1327_winstar_128x64_display_info);
+    return 1;
+  }
+  else if ( msg == U8X8_MSG_DISPLAY_INIT )
+  {
+    u8x8_d_helper_display_init(u8x8);
+    u8x8_cad_SendSequence(u8x8, u8x8_d_ssd1327_winstar_128x64_init_seq);    
+    return 1;
+  }
+  else if  ( msg == U8X8_MSG_DISPLAY_SET_FLIP_MODE )
+  {
+    if ( arg_int == 0 )
+    {
+      u8x8_cad_SendSequence(u8x8, u8x8_d_ssd1327_winstar_96x64_flip0_seq);
+      u8x8->x_offset = u8x8->display_info->default_x_offset;
+    }
+    else
+    {
+      u8x8_cad_SendSequence(u8x8, u8x8_d_ssd1327_winstar_96x64_flip1_seq);
+      u8x8->x_offset = u8x8->display_info->flipmode_x_offset;
+    }
+    return 1;
+  }
+  return 0;
+}
 
 /*=============================================*/
 /*  Seeedstudio Grove OLED 96x96 */
